bf442d84d099e153458185d6d755789134f23820
SS007979 <Shaule.Sachs@penfed.org>
2019-05-08 14:00:36 -0400

    Run in Eclipse

---
+++ mkdirSrcGen.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+FOR %t IN ( cucumber jbehave ) DO FOR %s IN ( ui tests ) DO CALL :AddDir org.agileware.natural.%t.%s
+FOR %t IN ( cucumber jbehave ) DO CALL :AddDir org.agileware.natural.%t
+GOTO :EOF
+:AddDir
+SET DEST=%1
+SET NEWDIR=src-gen
+IF NOT EXIST %DEST%\%NEWDIR% MKDIR %DEST%\%NEWDIR%
+:EOF
+EXIT /B 0
--- org.agileware.natural.cucumber.ui/src/org/agileware/natural/cucumber/ui/labeling/CucumberLabelProvider.java
+++ org.agileware.natural.cucumber.ui/src/org/agileware/natural/cucumber/ui/labeling/CucumberLabelProvider.java
@@ -103,7 +103,7 @@ public class CucumberLabelProvider extends DefaultEObjectLabelProvider {
 		return "annotation.gif";
 	}
 	
-	private static String merge(EList<String> strings) {
+	public static String merge(EList<String> strings) {
 		StringBuilder builder = new StringBuilder();
 		for (String string : strings) {
 			builder.append(string);
