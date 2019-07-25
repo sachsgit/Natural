3b78e4dbe8443fd4d0a3b2fbfc8c4feb3b4b854b
SS007979 <Shaule.Sachs@penfed.org>
2019-07-25 15:24:37 -0400

    Synch with DRP_TEST_PC1

--- mkdirSrcGen.bat
+++ mkdirSrcGen.bat
@@ -1,10 +1,11 @@
 @ECHO OFF
-FOR %t IN ( cucumber jbehave ) DO FOR %s IN ( ui tests ) DO CALL :AddDir org.agileware.natural.%t.%s
-FOR %t IN ( cucumber jbehave ) DO CALL :AddDir org.agileware.natural.%t
+FOR %%t IN ( cucumber jbehave ) DO FOR %%s IN ( ui tests ) DO CALL :AddDir org.agileware.natural.%%t.%%s
+FOR %%t IN ( cucumber jbehave ) DO CALL :AddDir org.agileware.natural.%%t
 GOTO :EOF
 :AddDir
 SET DEST=%1
 SET NEWDIR=src-gen
 IF NOT EXIST %DEST%\%NEWDIR% MKDIR %DEST%\%NEWDIR%
+IF EXIST %DEST%\%NEWDIR% TYPE nul > src-gen.md
 :EOF
 EXIT /B 0
