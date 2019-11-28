591505890a5843956845c792fac7d86d4ec1bd5b
sachsgit <sachs@atlantisuniverse.com>
2019-11-28 12:33:13 -0500

    Synching with SACHSSDESKTOP

--- mkdirSrcGen.bat
+++ mkdirSrcGen.bat
@@ -6,6 +6,9 @@ GOTO :EOF
 SET DEST=%1
 SET NEWDIR=src-gen
 IF NOT EXIST %DEST%\%NEWDIR% MKDIR %DEST%\%NEWDIR%
-IF EXIST %DEST%\%NEWDIR% TYPE nul > src-gen.md
+IF NOT EXIST %DEST%\%NEWDIR% GOTO :EOF
+ECHO. > src-gen.md
+ECHO # src-gen Placeholder >> src-gen.md
+ECHO. >> src-gen.md
 :EOF
 EXIT /B 0
